6d50fef90f054667d16c6c4a21d1de8b9869cda2
Stephen <stevegns42@gmail.com>
2023-02-26 15:33:26 -0600

    stuff

--- score.sql
+++ score.sql
@@ -1,11 +1,19 @@
+
+CREATE DATABASE score;
+
 USE score;
 
 CREATE TABLE User
 ( 
-
 FirstName CHAR(15),
+UserName CHAR(15), 
 TikScore INT,
 WhackAMoleScore INT, 
 Hangman DECIMAL(5,2),
-Memory DECIMAL(5,2)
+Memor DECIMAL(5,2),
+NumberGuesser INT 
 );
+
+INSERT INTO User 
+VALUES 
+('Stephen', 'sgans42', null, null, null, null, null); 
