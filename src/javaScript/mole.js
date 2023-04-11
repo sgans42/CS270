dc14252a11df09dadad52e02a3ff8ee75f7b2309
Stephen <stevegns42@gmail.com>
2023-04-11 17:10:38 -0500

    game start fixed

--- src/javaScript/mole.js
+++ src/javaScript/mole.js
@@ -1,12 +1,19 @@
-let score = 0;
-let molesLeft = 30;
-let popupLength = 3000; //set to 3000 as default
+const def_score = 0;
+const def_molesLeft = 30;
+const def_popupLength = 3000;
+const def_isGameRunning = false;
+
+let score = def_score;
+let molesLeft = def_molesLeft;
+let popupLength = def_popupLength; //set to 3000 as default
+let isGameRunning = def_isGameRunning;
 let hideTimeout;
 let clickable = false;
 
 function popUpRandomMole() {
   if (molesLeft <= 0) {
     document.querySelector('.sb__game-over').classList.remove('sb__game-over--hidden');
+		isGameRunning = false;
     return;
   }
 
@@ -35,23 +42,36 @@ function hideMole(mole) {
   setTimeout(popUpRandomMole, 500);
 }
 
+function restartGame() {
+	isGameRunning = true;
+
+  document.querySelector('.sb__game-over').classList.add('sb__game-over--hidden');
+  score = def_score;
+  molesLeft = def_molesLeft;
+  popupLength = def_popupLength; // Reset popupLength to the default value
+  document.querySelector('.sb__score').innerHTML = score;
+  document.querySelector('.sb__moles').innerHTML = molesLeft;
+  
+  // Start the game
+  setTimeout(popUpRandomMole, 0);
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-	const startGameBtn = document.querySelector('#start-game-btn');
-	startGameBtn.addEventListener('click', () => {
-		document.querySelector('.sb__game-over').classList.add('sb__game-over--hidden');
-		score = 0;
-  	molesLeft = 30;
-  	document.querySelector('.sb__score').innerHTML = score;
-  	document.querySelector('.sb__moles').innerHTML = molesLeft;
-		
+  const startGameBtn = document.querySelector('#start-game-btn');
+  startGameBtn.addEventListener('click', () => {
+		if(isGameRunning) { 
+			return;
+		}
     startGameBtn.classList.add('sb__start-game-btn--hidden');
-    setTimeout(popUpRandomMole, 0);
+    restartGame();
   });
 
   const moleHeads = document.querySelectorAll('.wgs__mole-head');
   for (let moleHead of moleHeads) {
     moleHead.addEventListener('click', event => {
-      if (!clickable) return;
+      if(!clickable) {
+				return;
+			}
 
       score += 1;
       document.querySelector('.sb__score').innerHTML = score;
@@ -61,7 +81,6 @@ window.addEventListener('DOMContentLoaded', () => {
       hideMole(event.target);
 
       event.target.classList.add('wgs__mole-head--hidden');
-
     });
   }
 });
